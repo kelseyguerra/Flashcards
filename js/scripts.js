e7cea1331be16c5b42607289f276d620b1888589
Ami Cooper and Kelsey guerra <kelsey@>
2018-03-20 11:52:18 -0700

    add text, edit javascript, style with css

---
+++ js/scripts.js
@@ -0,0 +1,95 @@
+$(document).ready(function() {
+  $("#Javascript").click(function() {
+    $("#Javascript-ans").show();
+  });
+});
+$(document).ready(function() {
+  $("#Operators").click(function() {
+    $("#Operators-ans").show();
+  });
+});
+$(document).ready(function() {
+  $("#Variablenaming").click(function() {
+    $("#Variablenaming-ans").show();
+  });
+});
+$(document).ready(function() {
+  $("#Variables").click(function() {
+    $("#Variables-ans").show();
+  });
+});
+$(document).ready(function() {
+  $("#Functions").click(function() {
+    $("#Functions-ans").show();
+  });
+});
+$(document).ready(function() {
+  $("#Methods").click(function() {
+    $("#Methods-ans").show();
+  });
+});
+$(document).ready(function() {
+  $("#Arguments").click(function() {
+    $("#Arguments-ans").show();
+  });
+});
+$(document).ready(function() {
+  $("#Parameters").click(function() {
+    $("#Parameters-ans").show();
+  });
+});
+$(document).ready(function() {
+  $("#Return").click(function() {
+    $("#Return-ans").show();
+  });
+});
+$(document).ready(function() {
+  $("#Chainingmethods").click(function() {
+    $("#Chainingmethods-ans").show();
+  });
+});
+$(document).ready(function() {
+  $("#Strings").click(function() {
+    $("#Strings-ans").show();
+  });
+});
+$(document).ready(function() {
+  $("#Booleans").click(function() {
+    $("#Booleans-ans").show();
+  });
+});
+$(document).ready(function() {
+  $("#Undefined").click(function() {
+    $("#Undefined-ans").show();
+  });
+});
+$(document).ready(function() {
+  $("#Nan").click(function() {
+    $("#Nan-ans").show();
+  });
+});
+$(document).ready(function() {
+  $("#Escape").click(function() {
+    $("#Escape-ans").show();
+  });
+});
+$(document).ready(function() {
+  $("#Alert").click(function() {
+    $("#Alert-ans").show();
+  });
+});
+$(document).ready(function() {
+  $("#Comments").click(function() {
+    $("#Comments-ans").show();
+  });
+});
+$(document).ready(function() {
+  $("#Jquery").click(function() {
+    $("#Jquery-ans").show();
+  });
+});
+$(document).ready(function() {
+  $("#Attributes").click(function() {
+    $("#Attributes-ans").show();
+  });
+});
